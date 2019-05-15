5b62f14b7f8bd3de9cc9799e9a8b57f0a4a36673
mashu.garg <mashu.garg@NOI-MGARG.nuance.com>
2019-05-15 15:54:06 +0530

    "Hashmap_hashset"

---
+++ HashmapHashset/src/Hashmap_Hashset.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+public class Hashmap_Hashset {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			HashSet<Integer> phones = new HashSet<Integer>();
+			phones.add(99999999);
+			phones.add(88888888);
+			phones.add(88888888);
+			phones.add(88888888);
+			System.out.println(phones);
+			
+	}
+
+}
